50774f161bb9489e54662fb10af1c5fe0ec72d07
archunar <you@example.com>
2024-03-05 20:05:53 +0530

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,11 @@ const Header = () => {
     return(
         <div className="Header">
 <div className="Header_Logo">
-    <a href="#app"><h2 className="Header_Name">Archuna</h2></a>
+    <a href="#app"><h2 className="Header_Name">Archunaa</h2></a>
 </div>
 <div className="Header_Container">
 <div className="Nav_Item"><a href="#about">About</a></div>
-<div className="Nav_Item"><a href="#Projects">Projects</a></div>
+<div className="Nav_Item"><a href="#projects">Projects</a></div>
 <div className="Nav_Item"><a href="#skills">Skills</a></div>
 </div>
 </div>
